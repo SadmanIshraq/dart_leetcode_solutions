50ef9f97216ce66e7a2424bc00e0ca1b9bf1f031
Sadman Ishraq <sadmanishraq789@gmail.com>
2024-07-01 17:11:29 +0600

    chore: Leetcode problem solution

---
+++ bin/problem9.dart
@@ -0,0 +1,16 @@
+//problem link below:
+/* https://leetcode.com/problems/two-sum/description/  */
+
+
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    for (var i = 0; i < nums.length; i++) {
+      for (var j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          return [i, j];
+        }
+      }
+    }
+    return nums;
+  }
+}
